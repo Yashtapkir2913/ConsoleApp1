45a9c0511b2cb886dc4bb7be0d085aa35592fe75
Yashtapkir2913 <yashsantoshtapkir2913@gmail.com>
2025-11-19 10:03:10 +0530

    Interface 1

---
+++ Interface/IDDriveable.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Interface
+{
+
+    public interface IDriveable
+    {
+        void Start();
+        void Forward();
+        void Turn(double angle);
+        void Stop();
+    }
+
+    public class Bicycle : IDriveable
+    {
+        public void Start()
+        {
+            Console.WriteLine("Bicycle started.");
+        }
+
+        public void Forward()
+        {
+            Console.WriteLine("Bicycle moving forward.");
+        }
+
+        public void Turn(double angle)
+        {
+            Console.WriteLine($"Bicycle turning by {angle} degrees.");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Bicycle stopped.");
+        }
+
+        public override string ToString()
+        {
+            return "This is a Bicycle.";
+        }
+    }
+
+
+    public class Car1 : IDriveable
+    {
+        public void Start()
+        {
+            Console.WriteLine("Car engine started.");
+        }
+
+        public void Forward()
+        {
+            Console.WriteLine("Car moving forward.");
+        }
+
+        public void Turn(double angle)
+        {
+            Console.WriteLine($"Car turning by {angle} degrees.");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Car stopped.");
+        }
+
+        public override string ToString()
+        {
+            return "This is a Car.";
+        }
+    }
+
+
+    class IDriveable1
+    {
+        public static void M1()
+        {
+            // Bicycle object
+            IDriveable bicycle = new Bicycle();
+            bicycle.Start();
+            bicycle.Forward();
+            bicycle.Turn(45);
+            bicycle.Stop();
+            Console.WriteLine(bicycle.ToString());
+            Console.WriteLine("---------------------");
+
+            // Car object
+            IDriveable car = new Car1();
+            car.Start();
+            car.Forward();
+            car.Turn(45);
+            car.Stop();
+            Console.WriteLine(car.ToString());
+            Console.WriteLine("---------------------");
+        }
+    }
+
+}//IDriveable1.M1();
+
---
+++ Interface/IInstrument.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Interface
+{
+
+    public interface IInstrument
+    {
+        void Play();
+        string What();
+        void Adjust();
+    }
+
+
+    public class Wind : IInstrument
+    {
+        public virtual void Play()
+        {
+            Console.WriteLine("Wind instrument is playing...");
+        }
+
+        public virtual string What()
+        {
+            return "Wind Instrument";
+        }
+
+        public virtual void Adjust()
+        {
+            Console.WriteLine("Adjusting Wind instrument...");
+        }
+    }
+
+    public class Percussion : IInstrument
+    {
+        public virtual void Play()
+        {
+            Console.WriteLine("Percussion instrument is playing...");
+        }
+
+        public virtual string What()
+        {
+            return "Percussion Instrument";
+        }
+
+        public virtual void Adjust()
+        {
+            Console.WriteLine("Adjusting Percussion instrument...");
+        }
+    }
+
+    public class Stringed : IInstrument
+    {
+        public virtual void Play()
+        {
+            Console.WriteLine("Stringed instrument is playing...");
+        }
+
+        public virtual string What()
+        {
+            return "Stringed Instrument";
+        }
+
+        public virtual void Adjust()
+        {
+            Console.WriteLine("Adjusting Stringed instrument...");
+        }
+    }
+
+    public class Woodwind : Wind
+    {
+        public override void Play()
+        {
+            Console.WriteLine("Woodwind instrument is playing...");
+        }
+
+        public override string What()
+        {
+            return "Woodwind Instrument";
+        }
+    }
+
+    public class Brass : Percussion
+    {
+        public override void Play()
+        {
+            Console.WriteLine("Brass instrument is playing...");
+        }
+
+        public override string What()
+        {
+            return "Brass Instrument";
+        }
+    }
+
+    class IInstrument1
+    {
+        public static void M1()
+        {
+            // Wind
+            IInstrument wind = new Wind();
+            wind.Play();
+            Console.WriteLine(wind.What());
+            wind.Adjust();
+            Console.WriteLine("--------------------------");
+
+            // Percussion
+            IInstrument percussion = new Percussion();
+            percussion.Play();
+            Console.WriteLine(percussion.What());
+            percussion.Adjust();
+            Console.WriteLine("--------------------------");
+
+            // Stringed
+            IInstrument stringed = new Stringed();
+            stringed.Play();
+            Console.WriteLine(stringed.What());
+            stringed.Adjust();
+            Console.WriteLine("--------------------------");
+
+            // Woodwind
+            IInstrument woodwind = new Woodwind();
+            woodwind.Play();
+            Console.WriteLine(woodwind.What());
+            woodwind.Adjust();
+            Console.WriteLine("--------------------------");
+
+            // Brass
+            IInstrument brass = new Brass();
+            brass.Play();
+            Console.WriteLine(brass.What());
+            brass.Adjust();
+            Console.WriteLine("--------------------------");
+        }
+    }
+
+}
